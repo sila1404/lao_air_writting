95c8c257d2b3b391308576fd89383509cf8298c1
sila1404 <silamany1404@gmail.com>
2025-01-19 17:52:54 +0700

    feat: initial OCR implementation

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -2,6 +2,7 @@ from .drawing_util import DrawingArea, DrawingCanvas
 from .hand_tracking import HandTracker
 from .model import CharacterRecognitionModel
 from .visualization import DisplayManager, VisualizationManager
+from .ocr import OCRProcessor
 
 __all__ = [
     "DrawingArea",
@@ -10,4 +11,5 @@ __all__ = [
     "CharacterRecognitionModel",
     "DisplayManager",
     "VisualizationManager",
+    "OCRProcessor",
 ]
